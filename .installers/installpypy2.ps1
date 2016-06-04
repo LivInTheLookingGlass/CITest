c1e784d9b1c0ac46b4dba3386b64c28478a0b34e
Gabe Appleton <gappleto97+development@gmail.com>
2016-06-03 17:20:15 -0700

    Make get-pip.py download on demand

--- .installers/installpypy2.ps1
+++ .installers/installpypy2.ps1
@@ -1,3 +1,4 @@
 \Python27\python -c "import urllib; urllib.urlretrieve ('http://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', r'pypy-4.0.1-win32.zip')"
 7z x pypy-4.0.1-win32.zip > Out-Null
+\Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
 pypy-4.0.1-win32\\pypy get-pip.py
