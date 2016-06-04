104dfc7e3aef354f3dc9765a289a2eb150f3ce7b
Gabe Appleton <gappleto97+development@gmail.com>
2016-06-04 00:54:22 -0700

    Remove more unneeded characters

--- .installers/installpypy2.ps1
+++ .installers/installpypy2.ps1
@@ -1,4 +1,3 @@
-python -c "import urllib; urllib.urlretrieve('http://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', 'pypy-4.0.1-win32.zip')"
+python -c "import urllib; urllib.urlretrieve('http://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', 'pypy-4.0.1-win32.zip'); urllib.urlretrieve('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"
 7z x pypy-4.0.1-win32.zip > Out-Null
-python -c "import urllib; urllib.urlretrieve('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"
 pypy-4.0.1-win32\pypy get-pip.py
