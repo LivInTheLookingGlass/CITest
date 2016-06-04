9ebc7c9c43070155641081bc2f49018ac43a2c4e
Gabe Appleton <gappleto97+development@gmail.com>
2016-06-04 00:46:34 -0700

    Remove unneeded characters [skip ci]

--- .installers/installpypy3.ps1
+++ .installers/installpypy3.ps1
@@ -1,5 +1,5 @@
-\Python27\python -c "import urllib; urllib.urlretrieve ('http://bitbucket.org/pypy/pypy/downloads/pypy3-2.4.0-win32.zip', r'pypy3-2.4.0-win32.zip')"
+\Python27\python -c "import urllib; urllib.urlretrieve('http://bitbucket.org/pypy/pypy/downloads/pypy3-2.4.0-win32.zip', 'pypy3-2.4.0-win32.zip')"
 7z x pypy3-2.4.0-win32.zip > Out-Null
-\Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
-pypy3-2.4.0-win32\\pypy get-pip.py
-pypy3-2.4.0-win32\\pypy -m pip install setuptools
+\Python27\python -c "import urllib; urllib.urlretrieve('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"
+pypy3-2.4.0-win32\pypy get-pip.py
+pypy3-2.4.0-win32\pypy -m pip install setuptools
